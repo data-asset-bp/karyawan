4ec46bc78df6769935052621b1a51641e4f562e6
Randi Julian Saputra <randisaputra978@gmail.com>
2022-10-31 16:24:39 +0700

    Update data_chekin.php

--- resources/views/checkin/data_chekin.php
+++ resources/views/checkin/data_chekin.php
@@ -61,15 +61,74 @@
                   </button>
                 </a>
               </td>
-              <td style="text-align: center ;"><a href="./apps/Exports/export_pdf_peminjaman1.php?id=<?= $id ?>">
-                  <button type="button" class="dcd-badge btn-success">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
-                      <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z" />
-                    </svg>
-                  </button>
-                </a>
-              </td>
+              <td>
+                <button type="button" class="dcd-badge btn-success" data-toggle="modal" data-target="#print<?= $no_asset; ?>">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
+                    <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z" />
+                  </svg>
+                </button>
+                <!-- Checkout The Modal -->
+                <div class="modal fade" id="print<?= $no_asset; ?>">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
 
+                      <!-- Modal Header -->
+                      <div class="modal-header">
+                        <h4 class="modal-title">Print Surat Peminjaman Asset</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                      </div>
+
+                      <!-- Modal body -->
+                      <form method="post">
+                        <div class="modal-body">
+                          <table style="width:100%">
+                            <tr>
+                              <td style="width:30%;text-align:left;">Peminjaman PC/NOTEBOOK</td>
+                              <td style="width:2%;text-align:left;">:</td>
+                              <td style="width:68%;text-align:justify;">
+                                <a href="./apps/Exports/export_pdf_peminjaman1.php?id=<?= $id ?>">
+                                  <button type="button" class="dcd-badge dcd-badge-success">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
+                                      <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z" />
+                                    </svg>
+                                  </button>
+                                </a>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td style="width:30%;text-align:left;">Peminjaman Bukan PC/NOTEBOOK</td>
+                              <td style="width:2%;text-align:left;line-height: 5">:</td>
+                              <td style="width:68%;text-align:justify;">
+                                <a href="./apps/Exports/export_pdf_peminjaman2.php?id=<?= $id ?>">
+                                  <button type="button" class="dcd-badge dcd-badge-success">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
+                                      <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z" />
+                                    </svg>
+                                  </button>
+                                </a>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td style="width:30%;text-align:left;">Pengembalian Asset</td>
+                              <td style="width:2%;text-align:left;">:</td>
+                              <td style="width:68%;text-align:justify;">
+                                <a href="./apps/Exports/export_pdf_pengembalian.php?id=<?= $id ?>">
+                                  <button type="button" class="dcd-badge dcd-badge-success">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
+                                      <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z" />
+                                    </svg>
+                                  </button>
+                                </a>
+                              </td>
+                            </tr>
+                          </table>
+                        </div>
+                      </form>
+
+                    </div>
+                  </div>
+                </div>
+              </td>
 
               <!-- The Modal -->
               <div class="modal fade" id="checkin<?= $id; ?>">
